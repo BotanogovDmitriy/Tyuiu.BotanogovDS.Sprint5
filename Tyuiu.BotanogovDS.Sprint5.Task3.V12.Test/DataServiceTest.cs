47883f1828f440549f04ede7497479ac4a046599
tyuiu.BotanogovDS@outlook.com <mr.botanogov@gmail.com>
2023-11-19 19:24:12 +0500

    Task 3

---
+++ Tyuiu.BotanogovDS.Sprint5.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+
+using Tyuiu.BotanogovDS.Sprint5.Task3.V12.Lib;
+
+namespace Tyuiu.BotanogovDS.Sprint5.Task3.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"D:\source\repos\Tyuiu.BotanogovDS.Sprint5\Tyuiu.BotanogovDS.Sprint5.Task3.V12\bin\Debug\OutPutFileTask3.bin";
+            FileInfo fi = new FileInfo(path);
+            bool fileex = fi.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileex);
+        }
+    }
+}
---
+++ Tyuiu.BotanogovDS.Sprint5.Task3.V12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BotanogovDS.Sprint5.Task3.V12.Lib;
+
+namespace Tyuiu.BotanogovDS.Sprint5.Task3.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Ботаногов Д. С. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Спринт #5                                                                *");
+            Console.WriteLine("*Тема: Потоковый метод записи данных в бинарный файл                      *");
+            Console.WriteLine("*Задание #3                                                               *");
+            Console.WriteLine("*Вариант #12                                                              *");
+            Console.WriteLine("*Выполнил: Ботаногов Дмитрий Сергеевич | ИСТНб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("*Дано выражение вычислить его значение при x = 3, результат сохранить в   *");
+            Console.WriteLine("*бинарный файл OutPutFileTask3.bin и вывести в консоль. Округлить до трех *");
+            Console.WriteLine("*знаков после запятой                                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ                                                          *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 3;
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine(res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
