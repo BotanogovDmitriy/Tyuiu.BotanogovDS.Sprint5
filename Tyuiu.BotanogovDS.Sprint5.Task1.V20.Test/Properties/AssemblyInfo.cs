097cb1bcd60c2a5c71bfe5e70251786b1c19487c
tyuiu.BotanogovDS@outlook.com <mr.botanogov@gmail.com>
2023-11-19 18:51:01 +0500

    Task 0

---
+++ Tyuiu.BotanogovDS.Sprint5.Task1.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BotanogovDS.Sprint5.Task1.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BotanogovDS.Sprint5.Task1.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9493d53d-7a0b-417e-9ba0-d52da412e311")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.BotanogovDS.Sprint5.Task1.V20/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BotanogovDS.Sprint5.Task1.V20.Lib;
+
+namespace Tyuiu.BotanogovDS.Sprint5.Task1.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue = -5;
+            int stopValue = 5;
+
+            DataService ds = new DataService();
+            
+            Console.Title = "Спринт #5 | Выполнил: Ботаногов Д. С. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант 20                                                              *");
+            Console.WriteLine("* Выполнил: Ботаногов Дмитрий Сергеевич | ИСТНб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ: Дана фунцкия, (произвести табулирование) f(x) на заданном      *");
+            Console.WriteLine("* диапозоне [-5; 5] с шагом -1. Произвести проверку деления на ноль.      *");
+            Console.WriteLine("* При делении на ноль вернуть значение 0.                                 *");
+            Console.WriteLine("*          5x + 2.5                                                       *");
+            Console.WriteLine("* F(x) = ------------ + 2x + cos(x)                                       *");
+            Console.WriteLine("*         xin(x) + 3                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
