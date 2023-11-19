ab1a205eeb7761ed9c714c487c55663d00b55844
tyuiu.BotanogovDS@outlook.com <mr.botanogov@gmail.com>
2023-11-19 18:28:49 +0500

    Add project files.

---
+++ Tyuiu.BotanogovDS.Sprint5.Task0.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BotanogovDS.Sprint5.Task0.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BotanogovDS.Sprint5.Task0.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8f0f4bc1-b17f-4f76-9578-883890a2652a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
